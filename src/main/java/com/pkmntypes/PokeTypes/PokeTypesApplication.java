e636529cbb6a5910caafedf51aab3a6c51c5d987
Alvin Xu <alvin3xu1@gmail.com>
2023-06-25 16:15:49 -0700

    starter files

---
+++ src/main/java/com/pkmntypes/PokeTypes/PokeTypesApplication.java
@@ -0,0 +1,13 @@
+package com.pkmntypes.PokeTypes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokeTypesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokeTypesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pkmntypes/PokeTypes/PokeTypesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pkmntypes.PokeTypes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokeTypesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
